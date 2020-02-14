f4286adf753c01c256d6f9c276169d3b7fb22cae
Gerald E. Fux <gerald.e.fux@gmail.com>
2020-02-14 17:07:25 +0000

    Add tests for say_hi function

---
+++ tests/say_hi_test.py
@@ -0,0 +1,23 @@
+# Copyright 2020 The TimeEvolvingMPO Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Tests for the time_evovling_mpo.say_hi module.
+"""
+
+import pytest
+
+from time_evolving_mpo import say_hi
+
+def test_say_hi():
+    say_hi()
