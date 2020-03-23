d6a876c7462acd8e828fa1bcb8ac7363b8cf4153
Gerald E. Fux <gerald.e.fux@gmail.com>
2020-03-23 12:45:35 +0100

    Sort modules in __init__ alphabetically

--- time_evolving_mpo/__init__.py
+++ time_evolving_mpo/__init__.py
@@ -4,13 +4,23 @@ ToDo: The TimeEvolvingMPO docstring!
 from time_evolving_mpo.version import __version__
 from time_evolving_mpo.say_hi import say_hi
 
+# -- Modules in alphabetical order --------------------------------------------
+
 from time_evolving_mpo.bath import Bath
-from time_evolving_mpo.system import System
+
 from time_evolving_mpo.control import Control
 
-from time_evolving_mpo.tempo import Tempo
-from time_evolving_mpo.tempo import TempoParameters
-from time_evolving_mpo.tempo import guess_tempo_parameters
+from time_evolving_mpo.dynamics import Dynamics
+from time_evolving_mpo.dynamics import distance
+from time_evolving_mpo.dynamics import norms
+
+from time_evolving_mpo.exceptions import NumericsError
+from time_evolving_mpo.exceptions import NumericsWarning
+
+from time_evolving_mpo.imports import import_dynamics
+from time_evolving_mpo.imports import import_process_tensor
+
+import time_evolving_mpo.operators
 
 from time_evolving_mpo.process_tensor import ProcessTensor
 from time_evolving_mpo.process_tensor import compute_process_tensor
@@ -19,17 +29,11 @@ from time_evolving_mpo.process_tensor import apply_system_to_process_tensor
 from time_evolving_mpo.process_tensor import ProcessTensorParameters
 from time_evolving_mpo.process_tensor import guess_process_tensor_parameters
 
-from time_evolving_mpo.dynamics import Dynamics
-from time_evolving_mpo.dynamics import distance
-from time_evolving_mpo.dynamics import norms
-
-from time_evolving_mpo.imports import import_dynamics
-from time_evolving_mpo.imports import import_process_tensor
-
 from time_evolving_mpo.spectral_density import CustomFunctionSD
 from time_evolving_mpo.spectral_density import StandardSD
 
-from time_evolving_mpo.exceptions import NumericsError
-from time_evolving_mpo.exceptions import NumericsWarning
+from time_evolving_mpo.system import System
 
-import time_evolving_mpo.operators
+from time_evolving_mpo.tempo import Tempo
+from time_evolving_mpo.tempo import TempoParameters
+from time_evolving_mpo.tempo import guess_tempo_parameters
