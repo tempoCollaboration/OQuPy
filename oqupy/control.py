16a108488d7a67155652cd051149f6fd118e2954
Gerald E. Fux <gerald.e.fux@gmail.com>
2021-12-05 21:02:19 +0000

    Create controls at single time steps

--- oqupy/control.py
+++ oqupy/control.py
@@ -15,30 +15,68 @@
 ToDo
 """
 
-from typing import Callable, Optional, Tuple, Union
+from typing import Callable, Dict, Optional, Text, Tuple, Union
 
+import numpy as np
 from numpy import ndarray
 
+from oqupy.base_api import BaseAPIClass
 
-class Control:
+
+class Control(BaseAPIClass):
     """
     ToDo
     """
-    def __init__(self, dimension: int) -> None:
-        """
-        ToDo
-        """
-        pass # ToDo
+    def __init__(
+            self,
+            dimension: int,
+            name: Optional[Text] = None,
+            description: Optional[Text] = None,
+            description_dict: Optional[Dict] = None) -> None:
+        """Creates a Control object. """
+        self._dimension = dimension
+        self._step_controls = {'pre':{}, 'post':{}}
+        self._time_controls = {'pre':{}, 'post':{}}
+        self._control_times = {'pre':np.array([]), 'post':np.array([])}
+        super().__init__(name, description, description_dict)
+
+    @property
+    def dimension(self):
+        """Hilbert space dimension of the controlled system. """
+        return self._dimension
 
     def add_single(
             self,
-            control_operation: ndarray,
             time: Union[int, float],
-            pre: Optional[bool] = True) -> None:
+            control_operation: ndarray,
+            post: Optional[bool] = False) -> None:
         """
         ToDo
         """
-        pass# ToDo
+        if post:
+            pre_post = 'post'
+        else:
+            pre_post = 'pre'
+
+        if isinstance(time, int):
+            steps = self._step_controls[pre_post].keys()
+            if time in steps:
+                self._step_controls[pre_post][time] = \
+                    control_operation @ self._step_controls[pre_post][time]
+            else:
+                self._step_controls[pre_post][time] = control_operation
+        elif isinstance(time, float):
+            if time in self._control_times[pre_post]:
+                self._time_controls[pre_post][time] = \
+                    control_operation @ self._time_controls[pre_post][time]
+            else:
+                self._time_controls[pre_post][time] = control_operation
+                times = np.append(self._control_times[pre_post], time)
+                times.sort()
+                self._control_times[pre_post] = times
+        else:
+            raise TypeError("Parameter `time` must be either int or float.")
+
 
     def add_continuous(
             self,
@@ -47,7 +85,7 @@ class Control:
         """
         ToDo
         """
-        pass# ToDo
+        raise NotImplementedError()
 
     def get_controls(
             self,
@@ -58,5 +96,44 @@ class Control:
         """
         ToDo
         """
-        pass # ToDo
-        return None, None
+        pre_control_bool = False
+        post_control_bool = False
+        pre_control = np.identity(self.dimension**2)
+        post_control = np.identity(self.dimension**2)
+
+        # -- pre time-stamp controls --
+        a = np.round((self._control_times['pre'] - start_time) / dt)
+        times = np.array(self._control_times['pre'])[np.nonzero(a==step)]
+        if len(times) > 0:
+            print(times)
+            pre_control_bool = True
+            pre_control = self._time_controls['pre'][times[0]] @ pre_control
+            for t in times[1:]:
+                pre_control = self._time_controls['pre'][t] @ pre_control
+
+        # -- pre step controls --
+        steps = self._step_controls['pre'].keys()
+        if step in steps:
+            pre_control_bool = True
+            pre_control = self._step_controls['pre'][step] @ pre_control
+
+        # -- post step controls --
+        steps = self._step_controls['post'].keys()
+        if step in steps:
+            post_control_bool = True
+            post_control = self._step_controls['post'][step] @ post_control
+
+        # -- post time-stamp controls --
+        a = np.round((self._control_times['post'] - start_time) / dt)
+        times = np.array(self._control_times['post'])[np.nonzero(a==step)]
+        if len(times) > 0:
+            post_control_bool = True
+            post_control = self._time_controls['post'][times[0]] @ post_control
+            for t in times[1:]:
+                post_control = self._time_controls['post'][t] @ post_control
+
+        if not pre_control_bool:
+            pre_control = None
+        if not post_control_bool:
+            post_control = None
+        return pre_control, post_control
