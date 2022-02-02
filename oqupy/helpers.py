22ff9a7278bf3c1346a46f0edc7d77f97fa76899
Gerald E. Fux <gerald.e.fux@gmail.com>
2022-02-02 12:49:02 +0000

    Update Copyright 2022

--- oqupy/helpers.py
+++ oqupy/helpers.py
@@ -1,4 +1,4 @@
-# Copyright 2021 The TEMPO Collaboration
+# Copyright 2022 The TEMPO Collaboration
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file  in compliance with the License.
