e3f4926ac6cb086a919709222eb6b798fd3f2f4b
Gerald E. Fux <gerald.e.fux@gmail.com>
2022-08-07 15:23:49 +0100

    Add test for PT-TEBD with different dimensions

--- tests/coverage/pt_tebd_test.py
+++ tests/coverage/pt_tebd_test.py
@@ -17,11 +17,12 @@ Tests for the time_evovling_mpo.pt_tebd module.
 
 import pytest
 
+import numpy as np
 import oqupy
 
 up_dm = oqupy.operators.spin_dm("z+")
-system_chain = oqupy.SystemChain(hilbert_space_dimensions=[2,2])
-initial_augmented_mps = oqupy.AugmentedMPS([up_dm, up_dm])
+system_chain = oqupy.SystemChain(hilbert_space_dimensions=[2,3])
+initial_augmented_mps = oqupy.AugmentedMPS([up_dm, np.diag([1,0,0])])
 pt_tebd_params = oqupy.PtTebdParameters(dt=0.2, order=2, epsrel=1.0e-4)
 
 def test_get_augmented_mps():
@@ -32,8 +33,10 @@ def test_get_augmented_mps():
         parameters=pt_tebd_params)
 
     augmented_mps = pt_tebd.get_augmented_mps()
-    assert augmented_mps.gammas[1].shape == (1,4,1,1)
+    assert augmented_mps.gammas[0].shape == (1,4,1,1)
+    assert augmented_mps.gammas[1].shape == (1,9,1,1)
 
-    pt_tebd.compute(end_step=1, progress_type='silent')
+    pt_tebd.compute(end_step=2, progress_type='silent')
     augmented_mps = pt_tebd.get_augmented_mps()
-    assert augmented_mps.gammas[1].shape == (1,4,1,1)
+    assert augmented_mps.gammas[0].shape == (1,4,1,1)
+    assert augmented_mps.gammas[1].shape == (1,9,1,1)
