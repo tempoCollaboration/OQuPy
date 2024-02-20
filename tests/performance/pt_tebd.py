b5d4c383ad3538e4381c3060e254e5013ce98ba6
Gerald E. Fux <gerald.e.fux@gmail.com>
2024-02-19 21:03:47 -0800

    Create some pt-tebd performance examples

--- tests/performance/pt_tebd.py
+++ tests/performance/pt_tebd.py
@@ -11,37 +11,52 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""
-Performance tests for PT-TEBD computations.
-"""
+# """
+# Performance tests for PT-TEBD computations.
+# """
+import sys
+sys.path.insert(0,'.')
+import os
 
+import dill
 import itertools
-# import numpy as np
-# import oqupy
+import numpy as np
+import time
 
+
+import oqupy
+import oqupy.operators as op
+
+
+PT_DIR_PATH = "./tests/data/process_tensors/"
 
 # -- Test A -------------------------------------------------------------------
-"""
-ToDo: Here should be some description of the performance tests scenario A.
-"""
+# """
+# ToDo: Here should be some description of the performance tests scenario A.
+# """
+#"spinBoson_alpha0.16_zeta1.0_T0.0_cutoff1.0expon_tcut4.0_dt04_steps06_epsrel15",
 
 parameters_A1 = [
-    ["boson_alpha0.08_zeta3.0_T0.0_gauss_dt0.04",
-     "boson_alpha0.16_zeta3.0_T0.0_gauss_dt0.04",
-     "boson_alpha0.32_zeta3.0_T0.0_gauss_dt0.04"], # process_tensor_name
-    [8],                                           # number_of_sites
-    ["XY", "XYZ"],                                 # model
-    [1.0e-5, 1.0e-6, 1.0e-7],                      # tebd_epsrel
+    ["spinBoson_alpha0.16_zeta1.0_T0.0_cutoff1.0expon_tcut4.0_dt04_steps06_epsrel15",
+     "spinBoson_alpha0.16_zeta1.0_T0.0_cutoff1.0expon_tcut4.0_dt04_steps06_epsrel16",
+     "spinBoson_alpha0.16_zeta1.0_T0.0_cutoff1.0expon_tcut4.0_dt04_steps06_epsrel17"], # process_tensor_name
+    [7],                             # number_of_sites
+    [2],                             # number_of_pts
+    ["XY"],                          # model
+    [2**(-15), 2**(-16), 2**(-17)],  # tebd_epsrel
 ]
 
 parameters_A2 = [
-    ["boson_alpha0.16_zeta3.0_T0.0_gauss_dt0.04"], # process_tensor_name
-    [4,8,12,16],                                   # number_of_sites
-    [1.0e-6],                                      # tebd_epsrel
+    ["spinBoson_alpha0.32_zeta1.0_T0.0_cutoff1.0expon_tcut4.0_dt04_steps06_epsrel16"], # process_tensor_name
+    [7],                     # number_of_sites
+    [0,1,2,3],               # number_of_pts
+    ["XY"],                  # model
+    [2**(-16)],              # tebd_epsrel
 ]
 
 def pt_tebd_performance_A(process_tensor_name,
                           number_of_sites,
+                          number_of_pts,
                           model,
                           tebd_epsrel):
     """
@@ -49,38 +64,72 @@ def pt_tebd_performance_A(process_tensor_name,
     ToDo: This function should return the most relevant information concerning
           performance of a computation.
     """
-    return NotImplemented
+    N = number_of_sites
+    K = number_of_pts
+    tebd_order = 2
 
 
-# -- Test B -------------------------------------------------------------------
-"""
-ToDo: Here should be some description of the performance tests scenario B.
-"""
+    # -- process tensor --
+    pt_file_path = os.path.join(PT_DIR_PATH, f"{process_tensor_name}.hdf5")
+    pt = oqupy.import_process_tensor(pt_file_path)
 
-parameters_B1 = [
-    ["boson_alpha0.16_zeta3.0_T0.8_gauss_dt0.04"], # left_process_tensor_name
-    ["boson_alpha0.16_zeta3.0_T1.6_gauss_dt0.04"], # right_process_tensor_name
-    [4,8,12,16],                                   # number_of_sites
-    [1.0e-6],                                      # tebd_epsrel
-]
+    # -- chain hamiltonian --
+    if model=="XY":
+        h = np.array([[0.0, 0.0, 1.0]]*N)
+        J = np.array([[0.96, 1.04, 0.0]]*(N-1))
+    elif model=="XYZ":
+        h = np.array([[0.0, 0.0, 1.0]]*N)
+        J = np.array([[1.3, 0.7, 1.2]]*(N-1))
+    else:
+        raise ValueError("Model '{model}' not implemented!")
 
-def pt_tebd_performance_B(left_process_tensor_name,
-                          right_process_tensor_name,
-                          number_of_sites,
-                          tebd_epsrel):
-    """
-    ToDo: implement computation B
-    ToDo: This function should return the most relevant information concerning
-          performance of a computation.
-    """
-    return NotImplemented
+    system_chain = oqupy.SystemChain(hilbert_space_dimensions=[2]*N)
+
+    for n in range(N):
+        for i, xyz in enumerate(["x", "y", "z"]):
+            system_chain.add_site_hamiltonian(
+                site=n,
+                hamiltonian=0.5*h[n, i]*op.sigma(xyz))
+    for n in range(N-1):
+        for i, xyz in enumerate(["x", "y", "z"]):
+            system_chain.add_nn_hamiltonian(
+                site=n,
+                hamiltonian_l=0.5*J[n, i]*op.sigma(xyz),
+                hamiltonian_r=0.5*op.sigma(xyz))
+
+    # -- initial state --
+    initial_augmented_mps = oqupy.AugmentedMPS(
+        [op.spin_dm("z+")] + [op.spin_dm("z-")] * (N-1))
+
+    # -- pt-tebd computation --
+    pt_tebd_params = oqupy.PtTebdParameters(
+        dt=pt.dt,
+        order=tebd_order,
+        epsrel=tebd_epsrel)
+
+    pt_tebd = oqupy.PtTebd(
+        initial_augmented_mps=initial_augmented_mps,
+        system_chain=system_chain,
+        process_tensors=[pt]*K+[None]*(N-K),
+        parameters=pt_tebd_params,
+        dynamics_sites=list(range(N)))
+
+    start_time = time.time()
+    result = pt_tebd.compute(len(pt))
+    result['walltime'] = time.time()-start_time
+    result['N'] = N
+    result['K'] = K
+    result['model'] = model
+    result['tebd_esprel'] = tebd_epsrel
+
+    return result
 
 
 # -----------------------------------------------------------------------------
 
 ALL_TESTS = [
-    (pt_tebd_performance_A, [parameters_A1,parameters_A2]),
-    (pt_tebd_performance_B, [parameters_B1]),
+    (pt_tebd_performance_A, [parameters_A1, parameters_A2]),
+    # (pt_tebd_performance_B, [parameters_B1, parameters_B2]),
 ]
 
 # -----------------------------------------------------------------------------
@@ -95,4 +144,10 @@ def run_all():
             results_list.append(results)
         results_list_list.append(results_list)
     return results_list_list
+
+# -----------------------------------------------------------------------------
+
+all_results = run_all()
+with open('./tests/data/temp/pt_tebd_results.pkl', 'wb') as f:
+    dill.dump(all_results, f)
 
