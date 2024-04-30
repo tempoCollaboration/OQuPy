1988389a3b7dded3e38166360ad8e6adb2309406
Gerald E. Fux <gerald.e.fux@gmail.com>
2024-04-30 13:45:34 +0200

    Add option to choose coupling op in test PT generation

--- tests/data/generate_pts.py
+++ tests/data/generate_pts.py
@@ -31,18 +31,53 @@ def spin_boson_pt_exists(name, pt_dir = PT_DIR_PATH):
       """Check the existence of a precomputed process tensor in the process tensor directory."""
       return os.path.isfile(os.path.join(pt_dir,f"{name}.hdf5"))
 
+def parameters_from_name(name):
+      res = re.match('spinBoson_alpha([\d.]+)_zeta([\d.]+)_T([\d.]+)_cutoff([\d.]+)([a-z]+)_tcut([\d.]+)_dt([\d]+)_steps([\d]+)_epsrel([\d]+)_*(.*)', name)
+      if res is None:
+          raise ValueError(f"Invalid PT name '{name}'")
+      coupling = res[10] if res[10]!= '' else 'z'  
+      p = {
+            'alpha':float(res[1]),
+            'zeta':float(res[2]),
+            'T':float(res[3]),
+            'cutoff':float(res[4]),
+            'cutoffType':res[5],
+            'tcut':float(res[6]),
+            'dtExp':int(res[7]),
+            'stepsExp':int(res[8]),
+            'epsrelExp':int(res[9]),
+            'coupling':coupling,
+      }
+      return p
+
+def bath_from_parameters(p):
+      cutoff_map = {'gss': 'gaussian', 'exp': 'exponential', 'hrd':'hard'}
+
+      try:
+          cutoff_type = cutoff_map[p['cutoffType']]
+      except KeyError:
+          raise ValueError(f"Cutoff Type {p['cutoffType']} not known.")
+
+      correlations = oqupy.PowerLawSD(alpha=p['alpha'],
+                                      zeta=p['zeta'],
+                                      cutoff=p['cutoff'],
+                                      cutoff_type=cutoff_type,
+                                      temperature=p['T'])
+      bath = oqupy.Bath(oqupy.operators.sigma(p['coupling'])/2.0, correlations)
+      return bath
+
 def generate_spin_boson_pt(name, pt_dir = PT_DIR_PATH):
       """
       Generate a spin boson process tensor from 'name' using PT-TEBD and write it into the
       process tensor directory 'pt_dir'.
 
       The name 'name' has to be of the form:
-      'spinBoson_alpha$.$$_zeta$.$_T$.$_cutOff$.$%%%_tcut$.$_dt$$_steps$$_epsrel$$'
+      'spinBoson_alpha$.$$_zeta$.$_T$.$_cutoff$.$%%%_tcut$.$_dt$$_steps$$_epsrel$$'
                       A---     B--  C--       D--E--     F--   G-      H-       I-
       where '$' are digits and '%' are lowercase letters.
 
       For example:
-      'spinBoson_alpha0.08_zeta1.0_T0.8_cutOff1.0exp_tcut2.0_dt04_steps06_epsrel15'
+      'spinBoson_alpha0.08_zeta1.0_T0.8_cutoff1.0exp_tcut2.0_dt04_steps06_epsrel15'
                       A---     B--  C--       D--E--     F--   G-      H-       I-
       is then, for example, translated into the following parameters:
       A ... alpha        = 0.08
@@ -56,9 +91,10 @@ def generate_spin_boson_pt(name, pt_dir = PT_DIR_PATH):
       I ... epsrel       = 2**(-15)
 
       """
-      res = re.match('spinBoson_alpha([\d.]+)_zeta([\d.]+)_T([\d.]+)_cutoff([\d.]+)([a-z]+)_tcut([\d.]+)_dt([\d]+)_steps([\d]+)_epsrel([\d]+)', name)
+      res = re.match('spinBoson_alpha([\d.]+)_zeta([\d.]+)_T([\d.]+)_cutoff([\d.]+)([a-z]+)_tcut([\d.]+)_dt([\d]+)_steps([\d]+)_epsrel([\d]+)_*(.*)', name)
       if res is None:
           raise ValueError(f"Invalid PT name '{name}'")
+      coupling = res[10] if res[10]!= '' else 'z'  
       p = {
             'alpha':float(res[1]),
             'zeta':float(res[2]),
@@ -69,6 +105,7 @@ def generate_spin_boson_pt(name, pt_dir = PT_DIR_PATH):
             'dtExp':int(res[7]),
             'stepsExp':int(res[8]),
             'epsrelExp':int(res[9]),
+            'coupling':coupling,
       }
 
       file_path = os.path.join(PT_DIR_PATH,f"{name}.hdf5")
@@ -90,7 +127,7 @@ def generate_spin_boson_pt(name, pt_dir = PT_DIR_PATH):
                                       cutoff=p['cutoff'],
                                       cutoff_type=cutoff_type,
                                       temperature=p['T'])
-      bath = oqupy.Bath(oqupy.operators.sigma("z")/2.0, correlations)
+      bath = oqupy.Bath(oqupy.operators.sigma(p['coupling'])/2.0, correlations)
       pt_tempo_parameters = oqupy.TempoParameters(dt=dt, tcut=p['tcut'], epsrel=epsrel)
 
       pt = oqupy.pt_tempo_compute(
